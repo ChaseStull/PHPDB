4296af3b23e0b70498602dac6bf5322e9303511d
ChaseStull <chaseastull@gmail.com>
2017-04-03 22:50:47 -0400

    Added get_array_from_handle()
    Added a function that returns an array from a previously opened read file handle.

--- phpdb.php
+++ phpdb.php
@@ -1,10 +1,34 @@
 <?php
+    //Gets an array from a previously opened file handle
+    function get_array_from_handle($handle)
+    {
+        $contents = fread($handle, filesize($handle));
+        $array = json_decode($contents, false, filesize($handle));
+        return $array;
+    }
+    function get_array_from_handle($handle, $with_keys)
+    {
+        $contents = fread($handle, filesize($handle));
+        $array = json_decode($contents, $with_keys, filesize($handle));
+        return $array;
+    }
+    function get_array_from_handle($handle, $with_keys, $close_handle)
+    {
+        $contents = fread($handle, filesize($handle));
+        $array = json_decode($contents, $with_keys, filesize($handle));
+        if($close_handle)
+        {
+            fclose($handle);
+        }
+        return $array;
+    }
+    //Creates a new database file
     function newdb($type, $name, $location)
     {
         switch($type)
         {
-            case "userAccountSystem":
-            $fileLoc = $location."/".$name.".json";
+            case "userAccountSystem", "UAS":
+            $fileLoc = $location."/".$name.".phpdb";
             $db = fopen($fileLoc, "w");
             $uas = array($name, "uas");
             $enc = json_encode($uas);
@@ -12,7 +36,7 @@
             fclose($db);
             break;
             case "custom":
-            $fileLoc = $location."/".$name.".json";
+            $fileLoc = $location."/".$name.".phpdb";
             $db = fopen($fileLoc, "w");
             $custom = array($name, "custom");
             $enc = json_encode($custom);
